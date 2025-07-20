a9cd3d4cdb587415911eeaf49eab4f430e7de49b
born-sorravit-nt <sorravit.t@pillars-nova.com>
2025-07-20 21:12:12 +0700

    feat: implement registration form with validation and dialog feedback

--- src/app/api/auth.api.ts
+++ src/app/api/auth.api.ts
@@ -2,7 +2,8 @@ import { IRegister } from "@/interfaces/register.interface";
 import httpClient from "@/utils/httpClient";
 
 const register = async (registerDto: IRegister) => {
-  const response = await httpClient().post("/auth/register", {
+  const path = `/common/users/register`;
+  const response = await httpClient().post(path, {
     ...registerDto,
   });
   return response.data;
